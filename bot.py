6368c34b4c2286b1a7a5f3d791c01f613c2c5d87
alwjyhnyazsrhan-blip <alwjyhnyazsrhan@gmail.com>
2026-05-23 00:43:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,22 @@
 from pyrogram import Client, filters
 
-# بياناتك الخاصة
-api_id = 3479415
+# بياناتك التي استخرجتها من my.telegram.org
+api_id = 34979415
 api_hash = "8a37c4085db8f17687c1d9dede24068"
-bot_token = "8914465158:AAH9uuW7VKIywlAH0aJZXU-R6fygul8RtJ4"
+bot_token = "8914465158:AAEoy6cxuvmsxne2aqATdPf_npZuZCDPEyQ"
 
-# إنشاء البوت
+# إعداد البوت
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-# معرفات القنوات
-SOURCE_CHANNEL = -1002496748420
-TARGET_CHANNEL = -1003769157498
+# المعرفات التي زودتني بها (مع إضافة -100 للقنوات)
+source_channel = -1002496748420
+target_channel = -1003769157498
 
-@app.on_message(filters.chat(SOURCE_CHANNEL))
-def forward_message(client, message):
-    # يقوم البوت بنسخ الرسالة (Copy) للقناة الوجهة
-    message.copy(chat_id=TARGET_CHANNEL)
+@app.on_message(filters.chat(source_channel))
+def copy_message(client, message):
+    # يقوم البوت بنسخ الرسالة للقناة الثانية
+    message.copy(chat_id=target_channel)
+    print("تم نسخ منشور جديد!")
 
-print("البوت يعمل الآن، قم بنشر شيء في القناة الأساسية للتجربة...")
+print("البوت يعمل الآن، بانتظار المنشورات...")
 app.run()
